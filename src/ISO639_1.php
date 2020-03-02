d32273e0fcb5ea9e84e40c3b93e2fe7007012f1e
Виктор Гвоздиков <starosta-91@ya.ru>
2020-03-02 14:07:35 +0300

    iso 639 1

---
+++ src/ISO639_1.php
@@ -0,0 +1,361 @@
+<?
+
+namespace ModuleBZ;
+
+class ISO639_1 {
+    /** @var String Абхазский */
+    const _AB = "ab";
+    /** @var String Аварский */
+    const _AV = "av";
+    /** @var String Авестийский */
+    const _AE = "ae";
+    /** @var String Азербайджанский */
+    const _AZ = "az";
+    /** @var String Аймара */
+    const _AY = "ay";
+    /** @var String Акан */
+    const _AK = "ak";
+    /** @var String Албанский */
+    const _SQ = "sq";
+    /** @var String Амхарский */
+    const _AM = "am";
+    /** @var String Английский */
+    const _EN = "en";
+    /** @var String Арабский */
+    const _AR = "ar";
+    /** @var String Армянский */
+    const _HY = "hy";
+    /** @var String Ассамский */
+    const _AS = "as";
+    /** @var String Афарский */
+    const _AA = "aa";
+    /** @var String Африкаанс */
+    const _AF = "af";
+    /** @var String Бамбара */
+    const _BM = "bm";
+    /** @var String Баскский */
+    const _EU = "eu";
+    /** @var String Башкирский */
+    const _BA = "ba";
+    /** @var String Белорусский */
+    const _BE = "be";
+    /** @var String Бенгальский */
+    const _BN = "bn";
+    /** @var String Бирманский */
+    const _MY = "my";
+    /** @var String Бислама */
+    const _BI = "bi";
+    /** @var String Болгарский */
+    const _BG = "bg";
+    /** @var String Боснийский */
+    const _BS = "bs";
+    /** @var String Бретонский */
+    const _BR = "br";
+    /** @var String Бходжпури */
+    const _BH = "bh";
+    /** @var String Валлийский */
+    const _CY = "cy";
+    /** @var String Венгерский */
+    const _HU = "hu";
+    /** @var String Венда */
+    const _VE = "ve";
+    /** @var String Волапюк */
+    const _VO = "vo";
+    /** @var String Волоф */
+    const _WO = "wo";
+    /** @var String Вьетнамский */
+    const _VI = "vi";
+    /** @var String Галисийский */
+    const _GL = "gl";
+    /** @var String Ганда */
+    const _LG = "lg";
+    /** @var String Гереро */
+    const _HZ = "hz";
+    /** @var String Гренландский */
+    const _KL = "kl";
+    /** @var String Греческий (новогреческий) */
+    const _EL = "el";
+    /** @var String Грузинский */
+    const _KA = "ka";
+    /** @var String Гуарани */
+    const _GN = "gn";
+    /** @var String Гуджарати */
+    const _GU = "gu";
+    /** @var String Гэльский */
+    const _GD = "gd";
+    /** @var String Датский */
+    const _DA = "da";
+    /** @var String Дзонг-кэ */
+    const _DZ = "dz";
+    /** @var String Дивехи (Мальдивский) */
+    const _DV = "dv";
+    /** @var String Зулу */
+    const _ZU = "zu";
+    /** @var String Иврит */
+    const _HE = "he";
+    /** @var String Игбо */
+    const _IG = "ig";
+    /** @var String Идиш */
+    const _YI = "yi";
+    /** @var String Индонезийский */
+    const _ID = "id";
+    /** @var String Интерлингва */
+    const _IA = "ia";
+    /** @var String Интерлингве */
+    const _IE = "ie";
+    /** @var String Инуктитут */
+    const _IU = "iu";
+    /** @var String Инупиак */
+    const _IK = "ik";
+    /** @var String Ирландский */
+    const _GA = "ga";
+    /** @var String Исландский */
+    const _IS = "is";
+    /** @var String Испанский */
+    const _ES = "es";
+    /** @var String Итальянский */
+    const _IT = "it";
+    /** @var String Йоруба */
+    const _YO = "yo";
+    /** @var String Казахский */
+    const _KK = "kk";
+    /** @var String Каннада */
+    const _KN = "kn";
+    /** @var String Канури */
+    const _KR = "kr";
+    /** @var String Каталанский */
+    const _CA = "ca";
+    /** @var String Кашмири */
+    const _KS = "ks";
+    /** @var String Кечуа */
+    const _QU = "qu";
+    /** @var String Кикуйю */
+    const _KI = "ki";
+    /** @var String Киньяма */
+    const _KJ = "kj";
+    /** @var String Киргизский */
+    const _KY = "ky";
+    /** @var String Китайский */
+    const _ZH = "zh";
+    /** @var String Коми */
+    const _KV = "kv";
+    /** @var String Конго */
+    const _KG = "kg";
+    /** @var String Корейский */
+    const _KO = "ko";
+    /** @var String Корнский */
+    const _KW = "kw";
+    /** @var String Корсиканский */
+    const _CO = "co";
+    /** @var String Коса */
+    const _XH = "xh";
+    /** @var String Курдский */
+    const _KU = "ku";
+    /** @var String Кхмерский */
+    const _KM = "km";
+    /** @var String Лаосский */
+    const _LO = "lo";
+    /** @var String Латинский */
+    const _LA = "la";
+    /** @var String Латышский */
+    const _LV = "lv";
+    /** @var String Лингала */
+    const _LN = "ln";
+    /** @var String Литовский */
+    const _LT = "lt";
+    /** @var String Луба-катанга */
+    const _LU = "lu";
+    /** @var String Люксембургский */
+    const _LB = "lb";
+    /** @var String Македонский */
+    const _MK = "mk";
+    /** @var String Малагасийский */
+    const _MG = "mg";
+    /** @var String Малайский */
+    const _MS = "ms";
+    /** @var String Малаялам */
+    const _ML = "ml";
+    /** @var String Мальтийский */
+    const _MT = "mt";
+    /** @var String Маори */
+    const _MI = "mi";
+    /** @var String Маратхи */
+    const _MR = "mr";
+    /** @var String Маршалльский */
+    const _MH = "mh";
+    /** @var String Мерянский */
+    const _ME = "me";
+    /** @var String Монгольский */
+    const _MN = "mn";
+    /** @var String Мэнский (Мэнкский) */
+    const _GV = "gv";
+    /** @var String Навахо */
+    const _NV = "nv";
+    /** @var String Науру */
+    const _NA = "na";
+    /** @var String Ндебеле северный */
+    const _ND = "nd";
+    /** @var String Ндебеле южный */
+    const _NR = "nr";
+    /** @var String Ндунга */
+    const _NG = "ng";
+    /** @var String Немецкий */
+    const _DE = "de";
+    /** @var String Непальский */
+    const _NE = "ne";
+    /** @var String Нидерландский (Голландский) */
+    const _NL = "nl";
+    /** @var String Норвежский */
+    const _NO = "no";
+    /** @var String Ньянджа */
+    const _NY = "ny";
+    /** @var String Нюнорск (новонорвежский) */
+    const _NN = "nn";
+    /** @var String Оджибве */
+    const _OJ = "oj";
+    /** @var String Окситанский */
+    const _OC = "oc";
+    /** @var String Ория */
+    const _OR = "or";
+    /** @var String Оромо */
+    const _OM = "om";
+    /** @var String Осетинский */
+    const _OS = "os";
+    /** @var String Пали */
+    const _PI = "pi";
+    /** @var String Пенджабский */
+    const _PA = "pa";
+    /** @var String Персидский */
+    const _FA = "fa";
+    /** @var String Польский */
+    const _PL = "pl";
+    /** @var String Португальский */
+    const _PT = "pt";
+    /** @var String Пушту */
+    const _PS = "ps";
+    /** @var String Ретороманский */
+    const _RM = "rm";
+    /** @var String Руанда */
+    const _RW = "rw";
+    /** @var String Румынский */
+    const _RO = "ro";
+    /** @var String Рунди */
+    const _RN = "rn";
+    /** @var String Русский */
+    const _RU = "ru";
+    /** @var String Самоанский */
+    const _SM = "sm";
+    /** @var String Санго */
+    const _SG = "sg";
+    /** @var String Санскрит */
+    const _SA = "sa";
+    /** @var String Сардинский */
+    const _SC = "sc";
+    /** @var String Свази */
+    const _SS = "ss";
+    /** @var String Сербский */
+    const _SR = "sr";
+    /** @var String Сингальский */
+    const _SI = "si";
+    /** @var String Синдхи */
+    const _SD = "sd";
+    /** @var String Словацкий */
+    const _SK = "sk";
+    /** @var String Словенский */
+    const _SL = "sl";
+    /** @var String Сомали */
+    const _SO = "so";
+    /** @var String Сото южный */
+    const _ST = "st";
+    /** @var String Суахили */
+    const _SW = "sw";
+    /** @var String Сунданский */
+    const _SU = "su";
+    /** @var String Тагальский */
+    const _TL = "tl";
+    /** @var String Таджикский */
+    const _TG = "tg";
+    /** @var String Таитянский */
+    const _TY = "ty";
+    /** @var String Тайский */
+    const _TH = "th";
+    /** @var String Тамильский */
+    const _TA = "ta";
+    /** @var String Татарский */
+    const _TT = "tt";
+    /** @var String Тви */
+    const _TW = "tw";
+    /** @var String Телугу */
+    const _TE = "te";
+    /** @var String Тибетский */
+    const _BO = "bo";
+    /** @var String Тигринья */
+    const _TI = "ti";
+    /** @var String Тонганский */
+    const _TO = "to";
+    /** @var String Тсвана */
+    const _TN = "tn";
+    /** @var String Тсонга */
+    const _TS = "ts";
+    /** @var String Турецкий */
+    const _TR = "tr";
+    /** @var String Туркменский */
+    const _TK = "tk";
+    /** @var String Узбекский */
+    const _UZ = "uz";
+    /** @var String Уйгурский */
+    const _UG = "ug";
+    /** @var String Украинский */
+    const _UK = "uk";
+    /** @var String Урду */
+    const _UR = "ur";
+    /** @var String Фарерский */
+    const _FO = "fo";
+    /** @var String Фиджи */
+    const _FJ = "fj";
+    /** @var String Филиппинский */
+    const _FL = "fl";
+    /** @var String Финский (Suomi) */
+    const _FI = "fi";
+    /** @var String Французский */
+    const _FR = "fr";
+    /** @var String Фризский */
+    const _FY = "fy";
+    /** @var String Фулах */
+    const _FF = "ff";
+    /** @var String Хауса */
+    const _HA = "ha";
+    /** @var String Хинди */
+    const _HI = "hi";
+    /** @var String Хиримоту */
+    const _HO = "ho";
+    /** @var String Хорватский */
+    const _HR = "hr";
+    /** @var String Церковнославянский (Старославянский) */
+    const _CU = "cu";
+    /** @var String Чаморро */
+    const _CH = "ch";
+    /** @var String Чеченский */
+    const _CE = "ce";
+    /** @var String Чешский */
+    const _CS = "cs";
+    /** @var String Чжуанский */
+    const _ZA = "za";
+    /** @var String Чувашский */
+    const _CV = "cv";
+    /** @var String Шведский */
+    const _SV = "sv";
+    /** @var String Шона */
+    const _SN = "sn";
+    /** @var String Эве */
+    const _EE = "ee";
+    /** @var String Эсперанто */
+    const _EO = "eo";
+    /** @var String Эстонский */
+    const _ET = "et";
+    /** @var String Яванский */
+    const _JV = "jv";
+    /** @var String Японский */
+    const _JA = "ja";
+
+}
